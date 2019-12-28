254450e65abb5cf673e91223a74239f53e91545b
katya feldman <katyapinich@gmail.com>
2019-12-28 15:12:51 +0200

    Adding management of days using polling
    Polling on each thread's status to verify they were all handled.
    Still sleeping for 1 second to make sure each thread's status has been
    set.
    Pretty sure it's not the best solution. But for now it works.

--- Hw_3/Hw_3/HotelManager.c
+++ Hw_3/Hw_3/HotelManager.c
@@ -12,6 +12,16 @@
 #include "HotelManager.h"
 #include "ThreadHandle.h"
 
+typedef enum
+{
+	THREAD_STATUS_NONE,
+	WAITING_TO_ENTER_ROOM,
+	IN_WORKING,
+	IN_WAITING_FOR_DAY,
+	OUT_DONE,
+	THREAD_STATUS_COUNT
+} GUEST_THREAD_STATUS;
+
 /*structs*/
 
 /*single thread input database*/
@@ -22,7 +32,13 @@ typedef struct _guest_thread_input
 	int num_of_rooms;
 	HANDLE *room_semaphore_handles;
 	char **path;
+	HANDLE guest_status_mutex_handle;
 } guest_thread_input_t;
+
+typedef struct _day_manager_input
+{
+	HANDLE guest_status_mutex_handle;
+} day_manager_input_t;
 
 /*global variables*/
 static const char DAY_PASSED_EVENT_NAME[] = "day_passed_event";
@@ -35,6 +51,7 @@ int guests_count;
 room_t rooms[NUM_OF_ROOMS];
 int rooms_count;
 int num_of_guests_out = 0;
+int guest_status_count[THREAD_STATUS_COUNT];
 
 /*Function declarations*/
 DWORD WINAPI guestThread(LPVOID argument, char *main_dir_path);
@@ -42,6 +59,8 @@ int findRoom(int budget, room_t rooms[], int num_of_rooms);
 EXIT_CODE updateRoomGuestCount(GUEST_STATUS guest_status, room_t *room);
 DWORD WINAPI dayManagerThread(LPVOID arguments);
 HANDLE getDayPassedEvent();
+EXIT_CODE updateGuestStatusCount(GUEST_THREAD_STATUS curr_guest_status, GUEST_THREAD_STATUS prev_guest_status, HANDLE guest_status_mutex_handle);
+EXIT_CODE countHandledGuests(HANDLE guest_status_mutex_handle, int *handled_guests_count);
 
 /*main tread - runs the program from main, receives directory*/
 EXIT_CODE runHotel(const char *main_dir_path)
@@ -56,13 +75,25 @@ EXIT_CODE runHotel(const char *main_dir_path)
 	int threads_count;
 	DWORD threads_wait_result;
 	HANDLE day_manager_handle;
+	HANDLE guest_status_mutex_handle;
+	day_manager_input_t day_manager_input;
+
+	// Initialize
 	day = 1;
+
 	exit_code = readRoomsFromFile(main_dir_path, rooms_filename, rooms, &rooms_count);
 	if (exit_code != HM_SUCCESS)
 		return exit_code;
 	exit_code = readGuestsFromFile(main_dir_path, guests_filename, guests, &guests_count);
 	if (exit_code != HM_SUCCESS)
 		return exit_code;
+
+	guest_status_count[THREAD_STATUS_NONE] = guests_count;
+	for (i = 1; i < THREAD_STATUS_COUNT; i++)
+	{
+		guest_status_count[i] = 0;
+	}
+
 	// Create mutexes for the rooms
 	for (i = 0; i < rooms_count; i++)
 	{
@@ -76,6 +107,7 @@ EXIT_CODE runHotel(const char *main_dir_path)
 			// Cleanup
 		}
 	}
+
 	log_file_mutex_handle = CreateMutex(
 		NULL,
 		FALSE,
@@ -84,6 +116,17 @@ EXIT_CODE runHotel(const char *main_dir_path)
 	{
 		// Cleanup
 	}
+
+	guest_status_mutex_handle = CreateMutex(
+		NULL,
+		FALSE,
+		"guest_status_mutex");
+	if (guest_status_mutex_handle == NULL)
+	{
+		// Clenup
+	}
+
+
 	// Create semaphores for the rooms
 	for (room_semaphores_count = 0; room_semaphores_count < rooms_count; room_semaphores_count++)
 	{
@@ -109,6 +152,8 @@ EXIT_CODE runHotel(const char *main_dir_path)
 		thread_inputs[threads_count].num_of_rooms = rooms_count;
 		thread_inputs[threads_count].room_semaphore_handles = room_semaphore_handles;
 		thread_inputs[threads_count].path = &main_dir_path;
+		thread_inputs[threads_count].guest_status_mutex_handle = guest_status_mutex_handle;
+
 		thread_handles[threads_count] = createThreadSimple(
 			(LPTHREAD_START_ROUTINE)guestThread,
 			(LPVOID)&(thread_inputs[threads_count]),
@@ -120,16 +165,18 @@ EXIT_CODE runHotel(const char *main_dir_path)
 			// Cleanup
 		}
 	}
+
+	day_manager_input.guest_status_mutex_handle = guest_status_mutex_handle;
 	thread_handles[threads_count] = createThreadSimple(
 		(LPTHREAD_START_ROUTINE)dayManagerThread, 
-		NULL, 
+		(LPVOID)&day_manager_input, 
 		NULL);
 	threads_wait_result = WaitForMultipleObjects(
 		guests_count + 1, 
 		thread_handles, 
 		TRUE, 
 		INFINITE);
-	printf_s("%d\n", day);
+	printf("%d\n", day);
 }
 
 
@@ -150,11 +197,19 @@ DWORD WINAPI guestThread(LPVOID argument)
 	DWORD event_wait_result;
 	const char *log_filename = "roomLog.txt";
 	thread_input = (guest_thread_input_t*)argument;
+
 	// Look for a room
 	room_index = findRoom(thread_input->guest.budget, thread_input->rooms, thread_input->num_of_rooms);
 	thread_input->guest.room_index = room_index;
-	// Use a semaphore to enter the room
+
+	// Update status to waiting
+	exit_code = updateGuestStatusCount(WAITING_TO_ENTER_ROOM, THREAD_STATUS_NONE, thread_input->guest_status_mutex_handle);
+	if (exit_code != HM_SUCCESS)
+		return exit_code;
+
 	thread_input->guest.status = GUEST_WAITING;
+
+	// Use a semaphore to enter the room
 	room_wait_result = WaitForSingleObject(room_semaphore_handles[room_index], INFINITE);
 	if (room_wait_result != WAIT_OBJECT_0)
 	{
@@ -162,6 +217,10 @@ DWORD WINAPI guestThread(LPVOID argument)
 	}
 
 	// Update number of guests in the room and update status
+	exit_code = updateGuestStatusCount(IN_WORKING, WAITING_TO_ENTER_ROOM, thread_input->guest_status_mutex_handle);
+	if (exit_code != HM_SUCCESS)
+		return exit_code;
+
 	thread_input->guest.status = GUEST_IN;
 	exit_code = updateRoomGuestCount(GUEST_IN, &(thread_input->rooms[room_index]));
 	//critical part - write to log file
@@ -182,9 +241,11 @@ DWORD WINAPI guestThread(LPVOID argument)
 	while (thread_input->guest.budget != 0)
 	{
 		day_passed_event_handle = getDayPassedEvent();
+		updateGuestStatusCount(IN_WAITING_FOR_DAY, IN_WORKING, thread_input->guest_status_mutex_handle);
 		event_wait_result = WaitForSingleObject(day_passed_event_handle, INFINITE);
 		ResetEvent(day_passed_event_handle);
 		// A day has passed - update budget
+		updateGuestStatusCount(IN_WORKING, IN_WAITING_FOR_DAY, thread_input->guest_status_mutex_handle);
 		thread_input->guest.budget = thread_input->guest.budget - thread_input->rooms[room_index].price;
 	}
 
@@ -193,7 +254,12 @@ DWORD WINAPI guestThread(LPVOID argument)
 	{
 		// Report error
 	}
+
 	//out of budget- getting out of the room
+	exit_code = updateGuestStatusCount(OUT_DONE, IN_WORKING, thread_input->guest_status_mutex_handle);
+	if (exit_code != HM_SUCCESS)
+		return exit_code;
+
 	thread_input->guest.status = GUEST_OUT;
 	exit_code = updateRoomGuestCount(GUEST_OUT, &(thread_input->rooms[room_index]));
 	//critical part - write to log file
@@ -262,27 +328,90 @@ EXIT_CODE updateRoomGuestCount(GUEST_STATUS guest_status, room_t *room)
 	return HM_SUCCESS;
 }
 
+EXIT_CODE updateGuestStatusCount(GUEST_THREAD_STATUS curr_guest_status, GUEST_THREAD_STATUS prev_guest_status, HANDLE guest_status_mutex_handle)
+{
+	DWORD handle_result;
+
+	handle_result = WaitForSingleObject(guest_status_mutex_handle, INFINITE);
+	if (handle_result != WAIT_OBJECT_0)
+	{
+		if (handle_result == WAIT_ABANDONED)
+			return HM_MUTEX_ABANDONED;
+		else
+			return HM_MUTEX_WAIT_FAILED;
+	}
+
+	/* Enter critical section */
+	guest_status_count[prev_guest_status]--;
+	guest_status_count[curr_guest_status]++;
+	/* End critical section */
+
+	handle_result = ReleaseMutex(guest_status_mutex_handle);
+	if (handle_result == FALSE)
+		return HM_MUTEX_RELEASE_FAILED;
+
+	return HM_SUCCESS;
+}
+
 /*changing and counting the days/ nights in the hotel, sends an event to all guests threads*/
 DWORD WINAPI dayManagerThread(LPVOID arguments)
 {
+	day_manager_input_t *thread_input;
 	int all_guests_handled = FALSE;
 	int handled_guests = 0;
+	int out_guests_count = 0;
 	HANDLE day_passed_event_handle;
 	BOOL is_success;
 
+	thread_input = (day_manager_input_t*)arguments;
+		
 	while (true)
 	{
-		Sleep(2000);
+		Sleep(1000);
 
-		day_passed_event_handle = getDayPassedEvent();
-		if (num_of_guests_out == guests_count)
+		countHandledGuests(thread_input->guest_status_mutex_handle, &handled_guests, &out_guests_count);
+		if (out_guests_count == guests_count)
 		{
-			return;
+			return HM_SUCCESS;
 		}
-		day++;
-		//event : day has passed
-		is_success = SetEvent(day_passed_event_handle);
+		else if (handled_guests == guests_count)
+		{
+			day_passed_event_handle = getDayPassedEvent();
+			day++;
+			printf("\n---- DAY %d ----\n", day);
+			//event : day has passed
+			is_success = SetEvent(day_passed_event_handle);
+		}
 	}
+}
+
+EXIT_CODE countHandledGuests(HANDLE guest_status_mutex_handle, int *handled_guests_count, int *out_guests_count)
+{
+	DWORD handle_result;
+
+	handle_result = WaitForSingleObject(guest_status_mutex_handle, INFINITE);
+	if (handle_result != WAIT_OBJECT_0)
+	{
+		if (handle_result == WAIT_ABANDONED)
+			return HM_MUTEX_ABANDONED;
+		else
+			return HM_MUTEX_WAIT_FAILED;
+	}
+
+	/* Enter critical section */
+	*handled_guests_count = 
+		guest_status_count[WAITING_TO_ENTER_ROOM] 
+		+ guest_status_count[IN_WAITING_FOR_DAY] 
+		+ guest_status_count[OUT_DONE];
+	*out_guests_count = guest_status_count[OUT_DONE];
+	printf("waiting for room = %d, waiting for day = %d, out = %d\n", guest_status_count[WAITING_TO_ENTER_ROOM], guest_status_count[IN_WAITING_FOR_DAY], guest_status_count[OUT_DONE]);
+	/* End critical section */
+
+	handle_result = ReleaseMutex(guest_status_mutex_handle);
+	if (handle_result == FALSE)
+		return HM_MUTEX_RELEASE_FAILED;
+
+	return HM_SUCCESS;
 }
 
 //for the day to get passed - handking the event
